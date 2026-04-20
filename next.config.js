8d030473823c0dfd393615b27ebe18b26c852671
Danylo <dashko950@gmail.com>
2026-04-20 20:36:35 +0200

    Fix build timeout issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Збільшуємо тайм-аут до 120 секунд (за замовчуванням 60)
+  staticPageGenerationTimeout: 120,
   images: {
     remotePatterns: [
       {
